31b9c8afbd59491e5f13be5cc056f430628a45a7
mdrmarcela <marcela.rodriguesccomput@gmail.com>
2025-11-22 16:30:18 -0300

    arrumando as pastas

--- app/schemas/cliente/login.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: 'object',
-  properties: {
-    email: { type: 'string' },
-    senha: { type: 'string' },
-  },
-  required: ['email', 'senha'],
-  additionalProperties: false,
-};

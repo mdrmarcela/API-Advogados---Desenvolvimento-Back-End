356466578a784b560d55d555279daca8b1a48fc8
mdrmarcela <marcela.rodriguesccomput@gmail.com>
2025-11-25 21:34:07 -0300

    Ajustando comentários

--- app/models/relations.js
+++ app/models/relations.js
@@ -1,7 +1,4 @@
-// models/relations.js
-// relacionamentos separados em arquivo próprio
 module.exports = function (models) {
-  // Advogado 1:N Processo
 models.advogado.hasMany(models.processo, {
   foreignKey: 'id_advogado',
   as: 'processos',

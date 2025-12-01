cd88a2809fe0423c2b5ab44fdd193ddd3c396ee3
mdrmarcela <marcela.rodriguesccomput@gmail.com>
2025-12-01 16:03:04 -0300

    Ajustando comentários

--- app/commons/helper.js
+++ app/commons/helper.js
@@ -3,13 +3,18 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 exports.hashSenha = (senha) => {
-  const hash = crypto.createHash('sha256'); //instancia de Hash
-  hash.update(senha); //atualiza o conteudo dele com a senha para processar
-  return hash.digest('hex'); //digest = resumo, valor do hash em hexadecimal
+  const hash = crypto.createHash('sha256'); //Cria um objeto hash usando o algoritmo sha256 
+  hash.update(senha); //atualiza o conteudo dele com a senha para processar (ou seja, pego a senha digitada pelo usuário e colono nele)
+  return hash.digest('hex'); 
 };
 
-exports.gerarTokenAcesso = (nome, id) => {
+exports.gerarTokenAcesso = (nome, id) => { //Gera token de autenticação, nele terá o nome e id do usuário
   return jwt.sign({ nome, id }, config.jwt.secret, {
-    expiresIn: config.jwt.expiration,
+    expiresIn: config.jwt.expiration, // define por quanto tempo o token é válido. 
   });
 };
+
+// No login: verifica email/senha, se estiver ok, chama gerarTokenAcesso para criar o token e devolver para o usuário.
+
+//Nas rotas protegidas: verifica se o token enviado pelo usuário é válido (usando jwt.verify) antes de permitir o acesso.
+//O cliente manda o token no POSTMAN. 
